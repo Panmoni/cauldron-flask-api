a4818fa475cd42a5d3e8cc24632e1163d6d726f1
George Donnelly <me@georgedonnelly.com>
2024-01-19 15:17:04 -0500

    update from working example

--- api/electrum_client.py
+++ api/electrum_client.py
@@ -1,15 +1,11 @@
 # electrum_client.py
-
 import json
 import socket
-
-
 class ElectrumClient:
     def __init__(self, server="rostrum.cauldron.quest", port=50001):
         self.s = socket.create_connection((server, port))
         self.f = self.s.makefile('r')
         self.id = 0
-
     def call(self, method, *args):
         req = {
             'id': self.id,
@@ -18,11 +14,8 @@ class ElectrumClient:
         }
         msg = json.dumps(req) + '\n'
         self.s.sendall(msg.encode('ascii'))
-
         # Read response
         response = self.f.readline()
         if not response:
             raise ValueError("Empty response from Electrum server")
-
         return json.loads(response)
-
--- api/index.py
+++ api/index.py
@@ -1,29 +1,16 @@
-# api/index.py
-
+# app.py
 from flask import Flask, jsonify, request
 from electrum_client import ElectrumClient
-
 app = Flask(__name__)
-
-# CORS(app)
-# cors = CORS(app, resources={'/*': {'origins': ['http://localhost:3000',
-#             'https://tokenstork.com', 'https://tokenstork-git-nextjs-apis-panmoni.vercel.app']}})
-# app.config['CORS_HEADERS'] = 'Content-Type'
-
-
 @app.route('/')
 def home():
     return 'Hello, World!'
-
-
 @app.route('/token_price', methods=['GET'])
 def token_price():
     category = request.args.get('category')
     decimals = request.args.get('decimals', type=int)
-
     if not category or decimals is None:
         return jsonify({'error': 'Category and decimals are required parameters'}), 400
-
     try:
         client = ElectrumClient()
         amount = 1 if decimals == 0 else 10 ** decimals
@@ -32,19 +19,16 @@ def token_price():
         return jsonify(response)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-
 @app.route('/token_liquidity', methods=['GET'])
 def token_liquidity():
     category = request.args.get('category')
-
     if not category:
         return jsonify({'error': 'Category is required'}), 400
-
     client = ElectrumClient()
     response = client.call("cauldron.contract.token_value_locked", 2, category)
-
     if not response:
         return jsonify({'error': 'Failed to fetch data from Electrum server'}), 500
-
     return jsonify(response)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
